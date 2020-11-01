4b8d963a54973c3067a472fd3a1ea8b0495cb79f
Klement Baastrup Johansen <kljo@aspit.dk>
2020-11-01 22:21:04 +0100

    ✨ Add about page

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <Header class="container"/>
+  <div class="wrapper">
+    <h1>Cykelbiblioteket</h1>
+    <p class="intro">
+      Gennem cykelbiblioteket kan man erfare cyklernes fulde potentiale og de forandringer, de kan medføre, i ens transportvaner og liv!
+    </p>
+    <p>Cykelbiblioteket repræsenterer et fuldgyldigt alternativ til biler. Det indeholder cykler, der nemt kan transportere store og tunge ting (ladcykler) og uden problemer kører lange distancer i høj hastighed (e-cykler og racercykler) eller nemt kan kombineres med offentlig transport (foldecykler).</p>
+    <h2>Hvad er cykelbibliotek</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nobis dolorum dolore ratione soluta aperiam eum reprehenderit iusto sapiente voluptatum!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto, reprehenderit ipsam, nulla alias delectus explicabo cumque quisquam, quibusdam laborum molestias facere! At ipsa unde tempore facilis perspiciatis culpa omnis aperiam.</p>
+    <picture class="full-bleed">
+      <img src="https://source.unsplash.com/1920x1200" alt="">
+    </picture>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione deleniti praesentium fugiat animi expedita pariatur.</p>
+    <h3>Køb af cykel gennem cykelbiblioteket</h3>
+    <p>Nu sælger vi også vores ‘cykelbiblioteks’-cykler – til virksomheder og offentlige institutioner. Fordi vi får cykler direkte fra en bred vifte af cykelleverandører, har vi også god mulighed for at hjælpe med at igangsætte og vedligeholde nye cykelbiblioteker.    </p>
+    <ul>
+      <li>test af en tekst i en liste</li>
+      <li>test af en tekst i en liste</li>
+      <li>test af en tekst i en liste</li>
+    </ul>
+    <p>Vores erfaring og undersøgelser viser, at cykelbiblioteket er et enestående sted at få hjælp til at opleve specialcyklers potentiale og finde ud af, om man vil investere i en. Når det kommer til dyrere cykler, vil de fleste gerne teste og sammenligne dem grundigt. Ved at prøve en specialcykel over en længere periode kan man opleve dens potentiale og opdage, at man ikke kan leve uden den</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.test-wrapper {
+    max-width: 65ch;
+    margin: 0 auto;
+}
+</style>
