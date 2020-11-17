5f9b24620a3424c037029c450f32e18e20248dab
Klement Baastrup Johansen <kljo@aspit.dk>
2020-11-17 22:25:12 +0100

    ✨ Add footer template

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+        <article>
+            <h2>Besøg os</h2>
+        </article>
+        <article></article>
+        <article></article>
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
