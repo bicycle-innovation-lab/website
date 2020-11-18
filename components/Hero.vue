33584a1bb2c6f897e324d347ad84f7c5d03cc24b
Klement Baastrup Johansen <kljo@aspit.dk>
2020-11-18 22:37:54 +0100

    🤡 Playing the the hero layout

--- components/Hero.vue
+++ components/Hero.vue
@@ -33,23 +33,43 @@ export default {}
 .bg {
   background: var(--red-color) url("../static/images/hero-image-halftone.jpg") no-repeat 60% bottom;
   height: 80vh;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+}
+
+.main {
+  h2,
+  p,
+  a {
+    opacity: 0;
+    animation: fade-in-top ease .4s forwards;
+  }
 }
 
 .main {
   display: flex;
-  max-width: 50%;
+  max-width: 500px;
+  flex: 1;
+  align-items: center;
 
   h2 {
-    font-size: 5rem;
-    line-height: 5rem;
+    font-size: 2.8rem;
+    line-height: 2.7rem;
+    animation-delay: .2s;
   }
 
-  picture {
-    align-self: flex-end;
-    img {
-      width: 100%;
-      object-fit: cover;
-    }
+  p {
+    font-size: 1.6rem;
+    line-height: 1.8rem;
+    animation-delay: .3s;
+  }
+
+  a{
+    animation-delay: .4s;
   }
 }
 
