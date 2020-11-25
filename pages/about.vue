17d73982a9647d33de831e3f97ee7a4f8ae09fc5
Klement Baastrup Johansen <kljo@aspit.dk>
2020-11-25 20:25:34 +0100

    ✨ Add media queries mixin

--- pages/about.vue
+++ pages/about.vue
@@ -1,40 +1,40 @@
 <template>
   <div>
     <Header class="container" />
-    <article class="grid">
+    <article class="grid sub-page">
       <header class="small-bleed">
         <h1>Cykel ting på Cuba som overskrift</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ullam.</p>
       </header>
-      <aside class="left-bleed">
+      <aside class="info-box left-bleed">
         <h2>Kontakt</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci, optio.</p>
       </aside>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ducimus molestiae officia natus! Ea soluta porro nostrum, hic necessitatibus eveniet veritatis fugiat vitae dicta modi molestias rem quia deleniti eos nihil ipsam atque neque ut odit quae iste vel rerum. Officia quae suscipit ipsum omnis, iusto, laboriosam sapiente voluptate recusandae in corporis reiciendis quod quia a dignissimos corrupti nesciunt maxime nostrum. Non perferendis temporibus sapiente. Adipisci amet totam libero incidunt cum laborum consequatur a ipsum suscipit nisi veritatis recusandae omnis earum asperiores, necessitatibus esse non dignissimos voluptas voluptatibus eaque reiciendis maxime sapiente, rem saepe. Corporis consequuntur non quisquam libero?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque harum recusandae quas nobis dolorem facilis fugiat eos maiores odit sequi!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore necessitatibus blanditiis quam, veritatis laudantium maiores consequuntur quas provident quod ipsa voluptas asperiores veniam, incidunt debitis accusamus aperiam. Asperiores doloremque ut voluptatibus? Accusamus deserunt rerum pariatur velit necessitatibus dolorem laudantium impedit?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odit, perferendis fugiat beatae magni accusamus nulla, alias facilis fugit quam, nam sint ipsa autem iste sit necessitatibus porro. Magnam, aliquid!</p>
       <figure class="full-bleed inner-grid">
-        <img class="full-bleed inner-grid" src="https://picsum.photos/1200" alt="">
-        <figcaption class="right text-small">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
-        </figcaption>
-      </figure>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellendus impedit nisi accusamus ducimus fugit culpa, commodi quod qui. Recusandae, est culpa vel perspiciatis quibusdam voluptatibus nostrum eaque iste optio illum, sequi aut repellendus labore dicta consequuntur? Asperiores, magni provident.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
-      <figure class="full-bleed inner-grid">
-        <img class="medium-bleed" src="https://picsum.photos/1000" alt="">
-        <figcaption class="right text-small">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
-        </figcaption>
-      </figure>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
-      <figure class="full-bleed inner-grid">
-        <img class="small-bleed" src="https://picsum.photos/1100" alt="">
+        <img class="small-bleed inner-grid" src="https://picsum.photos/1200" alt="">
         <figcaption class="right align-end text-small">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
         </figcaption>
       </figure>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ducimus molestiae officia natus! Ea soluta porro nostrum, hic necessitatibus eveniet veritatis fugiat vitae dicta modi molestias rem quia deleniti eos nihil ipsam atque neque ut odit quae iste vel rerum. Officia quae suscipit ipsum omnis, iusto, laboriosam sapiente voluptate recusandae in corporis reiciendis quod quia a dignissimos corrupti nesciunt maxime nostrum. Non perferendis temporibus sapiente. Adipisci amet totam libero incidunt cum laborum consequatur a ipsum suscipit nisi veritatis recusandae omnis earum asperiores, necessitatibus esse non dignissimos voluptas voluptatibus eaque reiciendis maxime sapiente, rem saepe. Corporis consequuntur non quisquam libero?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores voluptatem dolor sit eos fugiat debitis, quibusdam dolore corrupti ducimus iure quis corporis nemo placeat quia veritatis consequatur eius nesciunt. Error officia omnis perspiciatis neque. Corrupti, quaerat doloremque. Ex aperiam, recusandae minima quis error suscipit debitis autem maiores voluptates natus veritatis, sequi voluptas commodi libero obcaecati consequuntur dolor fuga aliquid?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
+      <figure class="full-bleed inner-grid">
+        <img class="full-bleed" src="https://picsum.photos/1000" alt="">
+        <figcaption class="right text-small">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
+        </figcaption>
+      </figure>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
+      <figure class="full-bleed inner-grid">
+        <img class="medium-bleed" src="https://picsum.photos/1100" alt="">
+        <figcaption class="right text-small">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
+        </figcaption>
+      </figure>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laborum sit molestias ullam provident consectetur eius officiis fuga vitae nulla eaque, aut laudantium dolore magnam facere. Incidunt nam ex earum!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ducimus molestiae officia natus! Ea soluta porro nostrum, hic necessitatibus eveniet veritatis fugiat vitae dicta modi molestias rem quia deleniti eos nihil ipsam atque neque ut odit quae iste vel rerum. Officia quae suscipit ipsum omnis, iusto, laboriosam sapiente voluptate recusandae in corporis reiciendis quod quia a dignissimos corrupti nesciunt maxime nostrum. Non perferendis temporibus sapiente. Adipisci amet totam libero incidunt cum laborum consequatur a ipsum suscipit nisi veritatis recusandae omnis earum asperiores, necessitatibus esse non dignissimos voluptas voluptatibus eaque reiciendis maxime sapiente, rem saepe. Corporis consequuntur non quisquam libero?</p>
     </article>
   </div>
