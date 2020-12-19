9c57180686a2bde5e349c3b7f3694b22f9e5845e
klementj <klementj@gmail.com>
2020-12-19 18:41:45 +0100

    🤡 Test images and text on subpages

--- pages/about.vue
+++ pages/about.vue
@@ -16,49 +16,27 @@
       <h2>Small-bleed</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore necessitatibus blanditiis quam, veritatis laudantium maiores consequuntur quas provident quod ipsa voluptas asperiores veniam, incidunt debitis accusamus aperiam. Asperiores doloremque ut voluptatibus? Accusamus deserunt rerum pariatur velit necessitatibus dolorem laudantium impedit?</p>
       <figure class="full-bleed grid">
-        <nuxt-image class="small-bleed grid" src="https://images.unsplash.com/photo-1608096299230-81c7b43d5dfc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80" />
-        <figcaption class="right align-end text-small">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
-        </figcaption>
-      </figure>
-
-      <h2>Medium-bleed</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore necessitatibus blanditiis quam, veritatis laudantium maiores consequuntur quas provident quod ipsa voluptas asperiores veniam, incidunt debitis accusamus aperiam. Asperiores doloremque ut voluptatibus? Accusamus deserunt rerum pariatur velit necessitatibus dolorem laudantium impedit?</p>
-      <figure class="full-bleed grid">
-        <nuxt-image class="small-bleed grid" src="https://picsum.photos/1200" />
+        <nuxt-image :placeholder="true" class="small-bleed grid" src="/images/pages/library/Lad-cykler.jpg" />
         <figcaption class="right align-end text-small">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
         </figcaption>
       </figure>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore necessitatibus blanditiis quam, veritatis laudantium maiores consequuntur quas provident quod ipsa voluptas asperiores veniam, incidunt debitis accusamus aperiam. Asperiores doloremque ut voluptatibus? Accusamus deserunt rerum pariatur velit necessitatibus dolorem laudantium impedit?</p>
-
-      <h2>Full-bleed</h2>
-      <figure class="full-bleed grid">
-        <nuxt-image class="small-bleed grid" src="https://picsum.photos/1200" />
-        <figcaption class="right align-end text-small">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
-        </figcaption>
-      </figure>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore necessitatibus blanditiis quam, veritatis laudantium maiores consequuntur quas provident quod ipsa voluptas asperiores veniam, incidunt debitis accusamus aperiam. Asperiores doloremque ut voluptatibus? Accusamus deserunt rerum pariatur velit necessitatibus dolorem laudantium impedit?</p>
-
-      <img class="small-bleed grid" src="https://picsum.photos/1200" alt="">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa asperiores voluptatem dolor sit eos fugiat debitis, quibusdam dolore corrupti ducimus iure quis corporis nemo placeat quia veritatis consequatur eius nesciunt. Error officia omnis perspiciatis neque. Corrupti, quaerat doloremque. Ex aperiam, recusandae minima quis error suscipit debitis autem maiores voluptates natus veritatis, sequi voluptas commodi libero obcaecati consequuntur dolor fuga aliquid?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
       <figure class="full-bleed grid">
-        <img class="full-bleed" src="https://picsum.photos/1000" alt="">
-        <figcaption class="right text-small">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
-        </figcaption>
-      </figure>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis assumenda exercitationem magni, laborum quas inventore maiores aspernatur iusto autem sunt at minus obcaecati quia laudantium adipisci corrupti. Consequuntur commodi esse pariatur unde obcaecati. Quaerat, voluptatibus.</p>
-      <figure class="full-bleed grid">
-        <img class="medium-bleed" src="https://picsum.photos/1100" alt="">
+        <nuxt-image :width="600" :height="600" class="full-bleed grid" src="/images/pages/library/Lad-cykler.jpg" />
         <figcaption class="right text-small">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
         </figcaption>
       </figure>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laborum sit molestias ullam provident consectetur eius officiis fuga vitae nulla eaque, aut laudantium dolore magnam facere. Incidunt nam ex earum!</p>
+      <figure class="full-bleed grid">
+        <nuxt-image :height="500" :width="1200" class="medium-bleed grid" src="/images/pages/library/Elektriske-cykler.jpg" />
+        <figcaption class="right text-small">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
+        </figcaption>
+      </figure>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ducimus molestiae officia natus! Ea soluta porro nostrum, hic necessitatibus eveniet veritatis fugiat vitae dicta modi molestias rem quia deleniti eos nihil ipsam atque neque ut odit quae iste vel rerum. Officia quae suscipit ipsum omnis, iusto, laboriosam sapiente voluptate recusandae in corporis reiciendis quod quia a dignissimos corrupti nesciunt maxime nostrum. Non perferendis temporibus sapiente. Adipisci amet totam libero incidunt cum laborum consequatur a ipsum suscipit nisi veritatis recusandae omnis earum asperiores, necessitatibus esse non dignissimos voluptas voluptatibus eaque reiciendis maxime sapiente, rem saepe. Corporis consequuntur non quisquam libero?</p>
     </article>
   </div>
--- pages/workshop.vue
+++ pages/workshop.vue
@@ -18,10 +18,18 @@
       <p>Vores værksted råder over et udvalg af professionelle værktøjer og de mest almindelige reservedele. Medlemmer kan også bestille knap så gængse reservedele til deres cykler fra større danske reservedelsgrossister, som vi har konto hos.</p>
       <p>Vi er også begyndt at samarbejde med andre lokale, ‘sociale’ cykelværksteder, primært gennem fælles bestilling af værktøj og reservedele samt afholdelse af workshops og indsamling af indleverede cykler fra genbrugscentre.</p>
       <h2>Åbne workshops</h2>
-      <p>Vi afholder regelmæssigt gratis workshops med erfarne mekanikere fra Butchers & Bicycles, Biomega og Flydende By UpCycles. Alle er velkomne til at deltage i disse workshops, som spænder fra gængse reparationer, som at skifte slidte dæk, til mere avancerede reparationer som hjulbygning eller udskiftning af tandhjul.</p>
+      <p>Vi afholder regelmæssigt gratis workshops med erfarne mekanikere fra <a href="https://www.butchersandbicycles.com/">Butchers & Bicycles</a>, <a href="https://biomega.com/">Biomega</a> og <a href="https://www.facebook.com/flydendebyupcycles/">Flydende By UpCycles</a>. Alle er velkomne til at deltage i disse workshops, som spænder fra gængse reparationer, som at skifte slidte dæk, til mere avancerede reparationer som hjulbygning eller udskiftning af tandhjul.</p>
+      <figure class="full-bleed grid">
+        <nuxt-image :placeholder="true" class="small-bleed grid" src="/images/pages/workshop/workshop-Omnium.jpg" />
+        <figcaption class="right align-end text-small">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias doloremque ea eaque, esse ratione consectetur qui aliquid animi id omnis repellat minus provident necessitatibus atque obcaecati molestias molestiae beatae! Sit.
+        </figcaption>
+      </figure>
       <p>Hvis du er cykelmekaniker og gerne vil stå for en workshop eller assistere i vores værksted, har du mulighed for at blive medlem i Bicycle Innovation Lab som frivillig. Læs mere her.</p>
       <h2>Udlån af cykel-workshop ekker kursus i reperations</h2>
       <p>Omfatter værktøj, reservedele, udstyr og en erfaren mekaniker, der efter aftale kan arrangere en skræddersyet cykel-workshop.</p>
+
+
     </article>
   </main>
 </template>
