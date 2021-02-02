aba6af01ae6f83dde28798569c92f5bdbba3ad2f
klementj <klementj@gmail.com>
2021-02-02 21:07:11 +0100

    🎨 Improve footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,65 +1,57 @@
 <template>
   <div class="bg grid">
+    <!-- Flex container -->
     <footer class="footer">
-      <div>
-        <header class="footer__header">
-          <h1>Bicycle Innovation Lab</h1>
-        </header>
+      <!-- Logo -->
+      <header class="footer__header">
+        <h1>Bicycle Innovation Lab</h1>
+      </header>
 
-        <section class="footer__navigation">
-          <article>
-            <h2>Projekter</h2>
-            <ul>
-              <li><a href="#">Cykelbibliotek</a></li>
-              <li><a href="www.google.com">Værksted</a> </li>
-              <li><a href="#">Aktivisme</a> </li>
-              <li><a href="#">Cykelkultur på Cuba</a> </li>
-              <li><a href="www.google.com">Undervisning</a> </li>
-              <li><a href="">Arangementer & Foredrag</a> </li>
-            </ul>
-          </article>
-          <article>
-            <h2>Kontakt os</h2>
-            <ul>
-              <li>
-                <address>
-                  Bicycle Innovation Lab </br>
-                  Prags Boulevard ved Prismen </br>
-                  Holmbladsgade 71 </br>
-                  2300 København S
-                </address>
-              </li>
-              <li>
-                info@bicycleinnovationlab.dk
-              </li>
-              <li>
-                <div class="header__social">
-                  <social-icons />
-                </div>
-              </li>
-            </ul>
-          </article>
-          <article>
-            <h2>Foreningen</h2>
-            <ul>
-              <li><a href="">Om os</a></li>
-              <li><a href="">Vedtægter</a></li>
-              <li><a href="">Vilkår</a></li>
-            </ul>
-          </article>
-        </section>
-      </div>
+      <!-- Memebership -->
+      <article class="footer__membership">
+        <div>
+          <p class="lead">
+            Bliv medlem, støt et godt formål og hjælp med at præge cykelkulturen allerede i dag.
+          </p>
+        </div>
+      </article>
 
-      <section class="footer__membership">
-        <h2>Bliv Medlem</h2>
-        <p>Eller start projekt, men en kort tekst her til en af de tre.</p>
-        <form action="">
-          <label for="email">Email</label>
-          <input id="email" type="email" placeholder="preben@email.dk">
-          <button>Tilmeld nyhedsbrev/</button>
-        </form>
-      </section>
+      <!-- Projects -->
+      <article class="footer__projects">
+        <h2>Projekter</h2>
+        <footer-links :links="[{content: 'Cykelbibliotek', to: '/library'}, {content: 'Værksted', to: '/workshop'}, {content: 'Aktivisme', to: '/activism'}, {content: 'Cykelkultur på Cuba', to: '/bicycle-culture-on-cuba'}, {content: 'Undervisning', to: '/teaching'}, {content: 'Arragementer & Foredrag', to: '/workshop'}]" />
+      </article>
 
+      <!-- Contact inforamtion like address, email and social -->
+      <article class="footer__contact">
+        <h2>Kontakt os</h2>
+        <ul>
+          <li>
+            <address>
+              Bicycle Innovation Lab </br>
+              Prags Boulevard ved Prismen </br>
+              Holmbladsgade 71 </br>
+              2300 København S
+            </address>
+          </li>
+          <li>
+            mail@bicycleinnovationlab.dk
+          </li>
+          <li>
+            <div class="footer__social">
+              <social-icons />
+            </div>
+          </li>
+        </ul>
+      </article>
+
+      <!-- The forening -->
+      <article class="footer__forening">
+        <h2>Foreningen</h2>
+        <footer-links :links="[{content: 'Om os', to: '/about'}, {content: 'Vedtægter', to: '/articles-of-association'}, {content: 'Vilkår', to: '/terms'}]" />
+      </article>
+
+      <!-- Copyright -->
       <section class="footer__copyright">
         &copy; 2021
       </section>
@@ -68,9 +60,12 @@
 </template>
 
 <script>
+import FooterLinks from './molecules/Footer-links.vue'
 import SocialIcons from './Social-icons.vue'
 
 export default {
-  components: { SocialIcons }
+  name: 'Footer',
+
+  components: { SocialIcons, FooterLinks }
 }
 </script>
